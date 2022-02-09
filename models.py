0b11570d305c4beadbddfe6295da3d37e059a225
IgorTerriaga <igsantos1996@gmail.com>
2022-02-09 10:20:59 -0300

    criação de outras tabelas

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Table, Boolean, Column, ForeignKey, Integer, String
 from database import Base
+from sqlalchemy.orm import relationship
 
 
 class Article(Base):
@@ -12,3 +13,24 @@ class Article(Base):
     newsSite = Column(String)
     summmary = Column(String)
     publishedAt = Column(String)
+
+    launches = relationship("Launche", back_populates="article")
+    events = relationship("Event", back_populates="article")
+
+
+class Launche(Base):
+    __tablename__ = "launches"
+    id = Column(String, primary_key=True, index=True)
+    provider = Column(String)
+    article_id = Column(Integer, ForeignKey("articles.id"))
+
+    article = relationship("Article", back_populates="launches")
+
+
+class Event(Base):
+    __tablename__ = "events"
+    id = Column(String, primary_key=True, index=True)
+    provider = Column(String)
+    article_id = Column(Integer, ForeignKey("articles.id"))
+
+    article = relationship("Article", back_populates="events")
