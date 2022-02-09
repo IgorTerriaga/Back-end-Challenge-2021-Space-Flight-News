d33234abee3bb31e38fcbc2575f0fd34d4d8a777
Igor Terriaga <igsantos1996@gmail.com>
2022-02-08 22:42:48 -0300

    arquivo de conexão com BD Postgres

---
+++ database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine, MetaData
+
+
+engine = create_engine(
+    "postgresql+psycopg2://zyhfesfxpfxvbz:6318d82135697351e26637859cc30126bd6937367c892daad9fc008ac6af06ba@ec2-34-205-46-149.compute-1.amazonaws.com:5432/d7b7etm394h0d",
+)
+meta = MetaData()
+conn  = engine.connect()
